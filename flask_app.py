18dbeb43cb38cebe50ee7599b2b92ad3af7e2b47
Laurentiu Raducu <laurentiu_raducu@iptiq.com>
2023-06-25 16:43:03 +0200

    new code

--- flask_app.py
+++ flask_app.py
@@ -21,8 +21,8 @@ openai.api_key = os.environ["OPENAI"]
 def chat_with_gpt(message):
     response = openai.ChatCompletion.create(
               model="gpt-3.5-turbo",
-              messages=[{"role": "system", "content": 'You are an world class automated API that handle text input. I want you to analyze the message that you get and check out if in the message there is any mention about victims, damaged hood, damaged windows, damaged doors, damaged tyres, damaged trunk or total loss. Respond only with a JSON file containing all these elements, and a numeric counter as a value for each JSON property. Do not respond using any text, like greetings or any of that stuff. Just respond with that JSON'},
-                        {"role": "user", "content": message}
+              messages=[{"role": "system", "content": 'You are an world class automated API that handle text input. I want you to analyze the message that you get and check out if in the message there is any mention about victims, damaged hood, damaged windows, damaged doors, damaged tyres, damaged trunk or total loss. Respond only with a JSON file containing all these elements, and a numeric counter as a value for each JSON property. Do not respond using any text, like greetings or any of that stuff. Just respond with that JSON. REMEMBER, just the JSON, nothing else! Absolutely no introduction, no other text, just the JSON!!!'},
+                        {"role": "user", "content": "This is the text message: " + message}
               ])
     return response["choices"][0]["message"]["content"]
 
@@ -47,20 +47,12 @@ def predict():
         damage_model = load_model('dmg_model.h5')
         predictions = damage_model.predict(preprocessed_image)
         prediction_result = predictions[0][0]
-        detected_image = Image.open('static/images/image0.jpg')
-        screenshot_image = Image.open('static/images/screenshot.jpg')
-        detected_image_size = detected_image.size
-        screenshot_image_size = screenshot_image.size
-        merged_image = Image.new('RGB',(5*screenshot_image_size[0],5*screenshot_image_size[1]), (250,250,250))
-        merged_image.paste(detected_image,(0,0))
-        merged_image.paste(screenshot_image,(detected_image_size[0],0))
-        merged_image.save("static/images/merged_image.jpg","JPEG")
         if prediction_result < predictions[0][1]:
             print("messy")
         else:
             print("clean")
         
-        return redirect("static/images/merged_image.jpg")
+        return redirect("static/images/image0.jpg")
 
     return render_template("index.html")
 
@@ -75,10 +67,9 @@ def transcribe():
     with sr.AudioFile(file) as source:
         audio_data = recognizer.record(source)
         text = recognizer.recognize_google(audio_data)
+        print(text)
 
     return jsonify(chat_with_gpt(text))
-
-    #return jsonify({'transcription': text})
 
 if __name__ == "__main__":
     
