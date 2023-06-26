edcc28e03f42f1ec962be4f0769e2cc991ef4045
Laurentiu Raducu <laurentiu_raducu@iptiq.com>
2023-06-26 16:19:50 +0200

    new code

--- flask_app.py
+++ flask_app.py
@@ -2,11 +2,14 @@ import argparse
 import io
 import os
 from PIL import Image
+import PIL.ExifTags
+from geopy.geocoders import Nominatim
 import torch
 from flask import Flask, render_template, request, redirect
 from keras.preprocessing import image
 from keras.applications.imagenet_utils import preprocess_input
 import numpy as np
+from datetime import datetime
 import speech_recognition as sr
 from tensorflow.keras.models import load_model
 from flask import Flask, render_template, request, redirect, jsonify
@@ -14,9 +17,23 @@ import urllib.request
 from werkzeug.utils import secure_filename
 import openai
 
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+EXIF_DATE_FORMAT = "%Y:%m:%d %H:%M:%S"
+
 app = Flask(__name__)
 
 openai.api_key = os.environ["OPENAI"]
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def get_country(latitude, longitude):
+    geolocator = Nominatim(user_agent="myGeocoder")
+    location = geolocator.reverse([latitude, longitude], exactly_one=True)
+    address = location.raw['address']
+    country = address.get('country', '')
+    return country
 
 def chat_with_gpt(message):
     response = openai.ChatCompletion.create(
@@ -25,6 +42,89 @@ def chat_with_gpt(message):
                         {"role": "user", "content": "This is the text message: " + message}
               ])
     return response["choices"][0]["message"]["content"]
+
+@app.route("/formdata", methods=["POST"])
+def post_formdata():
+    # Check if the post request has the file part
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part in the request.'}), 400
+
+    file = request.files['file']
+
+    # If user does not select file, browser submits an empty part without filename
+    if file.filename == '':
+        return jsonify({'error': 'No selected file.'}), 400
+
+    if file and allowed_file(file.filename):
+        # secure the filename before storing it directly on the filesystem
+        filename = secure_filename(file.filename)
+        file_path = os.path.join('static/save/', filename)
+        file.save(file_path)
+
+        policy_start_date = request.form.get('policyStartDate')
+        policy_end_date = request.form.get('policyEndDate')
+        country = request.form.get('country')
+
+        # Parse dates and check validity
+        try:
+            start_date = datetime.strptime(policy_start_date, "%Y-%m-%d").date()
+            end_date = datetime.strptime(policy_end_date, "%Y-%m-%d").date()
+            if start_date >= end_date:
+                return jsonify({'error': 'Policy start date must be before end date.'}), 400
+        except ValueError:
+            return jsonify({'error': 'Incorrect date format, should be YYYY-MM-DD.'}), 400
+
+        # Fetch the EXIF data from the image
+        img = PIL.Image.open(file_path)
+        exif_data = img._getexif()
+        
+
+        if exif_data is not None:
+            photo_date = None
+            lat_ref = None
+            lon_ref = None
+            lat_val = None
+            lon_val = None
+            
+            for tag, value in exif_data.items():
+                print(tag)
+                if tag == PIL.ExifTags.TAGS.get('DateTimeOriginal') or tag == PIL.ExifTags.TAGS.get('DateTime') or tag == 306 :
+                    photo_date = datetime.strptime(value, EXIF_DATE_FORMAT).date()
+                    print(photo_date)
+                elif tag == PIL.ExifTags.TAGS.get('GPSInfo'):
+                    for t, v in getattr(value, 'items', lambda: {})():
+                        if t == PIL.ExifTags.GPSTAGS.get('GPSLatitudeRef'):
+                            lat_ref = v
+                        elif t == PIL.ExifTags.GPSTAGS.get('GPSLongitudeRef'):
+                            lon_ref = v
+                        elif t == PIL.ExifTags.GPSTAGS.get('GPSLatitude'):
+                            lat_val = v
+                        elif t == PIL.ExifTags.GPSTAGS.get('GPSLongitude'):
+                            lon_val = v
+            if photo_date is not None:
+                if photo_date < start_date or photo_date > end_date:
+                    return jsonify({'error': 'Photo date is not within policy period.'}), 400
+            if lat_ref and lon_ref and lat_val and lon_val:
+                latitude = lat_val[0] + lat_val[1] / 60 + lat_val[2] / 3600
+                if lat_ref == 'S':
+                    latitude = -latitude
+                longitude = lon_val[0] + lon_val[1] / 60 + lon_val[2] / 3600
+                if lon_ref == 'W':
+                    longitude = -longitude
+                photo_country = get_country(latitude, longitude)
+                if photo_country != country:
+                    return jsonify({'error': 'Photo location does not match policy country.'}), 400
+
+        # Process the data...
+        return jsonify({
+            'filename': filename,
+            'policyStartDate': policy_start_date,
+            'policyEndDate': policy_end_date,
+            'country': country
+        })
+
+    return jsonify({'error': 'Allowed file types are png, jpg, jpeg.'}), 400
+
 
 @app.route("/", methods=["GET", "POST"])
 def predict():
@@ -73,7 +173,7 @@ def transcribe():
 
 if __name__ == "__main__":
     
-    parser = argparse.ArgumentParser(description="Flask app exposing yolov5 models")
+    parser = argparse.ArgumentParser(description="Damage Detective")
     parser.add_argument("--port", default=5000, type=int, help="port number")
     args = parser.parse_args()
 
